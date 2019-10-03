3f3853cefecfa5d93f587106b6394279c5e51d87
SoftwareEngineerChris <4376956+SoftwareEngineerChris@users.noreply.github.com>
2019-10-03 20:51:46 +0100

    Adds jazzy documentation for MonetaryExchange

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 [![Build Status](https://app.bitrise.io/app/57e424b934229804/status.svg?token=zDHT8jgVf-wPoK5oVp7LcA&branch=master)](https://app.bitrise.io/app/57e424b934229804) 
 [![SPM](https://img.shields.io/badge/SPM-Supported-informational)](#)
+[![Swift Version](https://img.shields.io/badge/Swift%20Version-5.1-informational)](#)
+
+`Exchange` provides exchange rates between currencies. It can also convert `MonetaryAmount` values into
+those of different `Currency` values.
 
 ## Installation
 
@@ -11,3 +15,23 @@ dependencies: [
     .package(url: "https://github.com/SoftwareEngineerChris/MonetaryExchange.git", from: "1.0.0")
 ]
 ```
+
+## Decoding a Fixer.io JSON response
+An `Exchange` can be decoded directly from a standard [Fixer.io](https://fixer.io/) _Latest Rates_ JSON response.
+See [Fixer API Documentation](https://fixer.io/documentation#latestrates) for more information about its API usage.
+
+### Example
+
+    let exchange = try? JSONDecoder().decode(Exchange.self, from: fixerResponseData)
+
+Alternatively, an `Exchange` can be constructed with a base currency and a dictionary of currency-rate pairs.
+
+## Cross-rates
+
+If converting between two currencies which neither are the base currency, but each have a rate against the base currency,
+then a cross-rate will be produced.
+
+For example, if the base currency is _EUR_ but a rate for _GBP to USD_ is requested, a cross-rate will be used. i.e. _GBP to EUR to USD_.
+
+See the Collins Dictionary definition of [Cross-Rate](https://www.collinsdictionary.com/dictionary/english/cross-rate)
+for more information.
--- Sources/MonetaryExchange/MonetaryExchange.swift
+++ Sources/MonetaryExchange/MonetaryExchange.swift
@@ -10,23 +10,73 @@ import Foundation
 import RoundedDecimal
 import MonetaryAmount
 
-public struct Exchange {
+/// `Exchange` provides exchange rates between currencies. It can also convert `MonetaryAmount` values into
+/// those of different `Currency` values.
+///
+/// ## Decoding a Fixer.io JSON response
+/// An `Exchange` can be decoded directly from a standard [Fixer.io](https://fixer.io/) _Latest Rates_ JSON response.
+/// See [Fixer API Documentation](https://fixer.io/documentation#latestrates) for more information about its API usage.
+///
+/// ### Example
+///
+///     let exchange = try? JSONDecoder().decode(Exchange.self, from: fixerResponseData)
+///
+/// Alternatively, an `Exchange` can be constructed with a base currency and a dictionary of currency-rate pairs.
+///
+/// ## Cross-rates
+///
+/// If converting between two currencies which neither are the base currency, but each have a rate against the base currency,
+/// then a cross-rate will be produced.
+///
+/// For example, if the base currency is _EUR_ but a rate for _GBP to USD_ is requested, a cross-rate will be used. i.e. _GBP to EUR to USD_.
+///
+/// See the Collins Dictionary definition of [Cross-Rate](https://www.collinsdictionary.com/dictionary/english/cross-rate)
+/// for more information.
+public struct Exchange: Decodable {
     
+    // MARK: Types
+    
+    /// Represents a monetary exchange rate between two currencies. Rounded to six decimal places.
     public typealias Rate = RoundedDecimal<Places.six>
     
-    let base: Currency
-    let rates: [Currency: Rate]
+    /// The possible errors thrown by an `Exchange`
+    public enum ExchangeError: Error {
+        /// Thrown when unable to decode an `Exchange` representation
+        case decodeError
+        /// Thrown when there is no exchage rate for a particular `Currency`
+        case noRateError
+    }
     
+    // MARK: Values
+    
+    /// The base currency for this `Exchange`. The currency to which all rates from this exchange are based upon.
+    ///
+    
+    public let base: Currency
+    
+    /// A dictionary of `Currency` values to `Rate` values where the `Rate` is the exchange rate between the base `Currency` and the queried `Currency`.
+    public let rates: [Currency: Rate]
+    
+    // MARK: Construction
+    
+    /// Creates a new `Exchange` from a base `Currency` and a collection of exchange rates.
+    ///
+    /// - Parameters:
+    ///
+    ///   - base: The base currency for this `Exchange`. The currency to which all rates from this exchange are based upon.
+    ///   - rates: A dictionary of `Currency` values to `Rate` values where the `Rate` is the exchange rate between the base `Currency` and the queried `Currency`.
     public init(base: Currency, rates: [Currency: Rate]) {
         self.base = base
         self.rates = rates
     }
     
-    public enum ExchangeError: Error {
-        case decodeError
-        case noRateError
-    }
+    // MARK: Exchanging rates and MonetaryAmounts
     
+    /// - Parameters:
+    ///     - originCurrency: The "base" `Currency` in the pair.
+    ///     - targetCurrency: The "quote" `Currency` in the pair.
+    /// - Throws: If the exchange has no exchange rate for the `targetCurrency`, this method will throw `ExchangeError.noRateError`
+    /// - Returns: The exchange rate for the provided currency pair.
     public func rate(from originCurrency: Currency, targetCurrency: Currency) throws -> Rate {
         
         if originCurrency == targetCurrency {
@@ -45,6 +95,11 @@ public struct Exchange {
         return rateFromBaseToTarget / rateFromBaseToOrigin
     }
     
+    /// - Parameters:
+    ///     - monetaryAmount: A `MonetaryAmount` which has a `Currency` to be converted to the `targetCurrency`
+    ///     - targetCurrency: The "quote" `Currency` in the pair (the base being that in the `monetaryAmount` parameter).
+    /// - Throws: If the exchange has no exchange rate for the `targetCurrency`, this method will throw `ExchangeError.noRateError`
+    /// - Returns: A `MonetaryAmount` representing the original amount converted to the target currency using the `Exchange` exchange rate.
     public func convert(monetaryAmount: MonetaryAmount, to targetCurrency: Currency) throws -> MonetaryAmount {
        
         let targetRate = try rate(from: monetaryAmount.currency, targetCurrency: targetCurrency)
@@ -52,13 +107,21 @@ public struct Exchange {
     }
 }
 
-extension Exchange: Decodable {
+extension Exchange {
+    
+    // MARK: Decodable
     
     enum CodingKeys: String, CodingKey {
         case base
         case rates
     }
     
+    /// An `Exchange` can be decoded directly from a standard standard [Fixer.io](https://fixer.io/) _Latest Rates_ JSON response.
+    /// See [Fixer API Documentation](https://fixer.io/documentation#latestrates) for more information about its API usage.
+    ///
+    /// #### Example
+    ///
+    ///     let exchange = try? JSONDecoder().decode(Exchange.self, from: fixerResponseData)
     public init(from decoder: Decoder) throws {
         
         let container = try decoder.container(keyedBy: CodingKeys.self)
